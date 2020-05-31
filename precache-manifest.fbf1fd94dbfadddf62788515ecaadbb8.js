21b42495b7542de563f3df0e7efb153774b14f7d
Gerald Torres <geraldjtorres@gmail.com>
2020-05-31 14:06:19 +0400

    Updates

---
+++ precache-manifest.fbf1fd94dbfadddf62788515ecaadbb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2b58ff74784c2f0580ee3d2c52ba44",
+    "url": "/google-places-test/index.html"
+  },
+  {
+    "revision": "4831be0d52d23079919f",
+    "url": "/google-places-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/google-places-test/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google-places-test/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4831be0d52d23079919f",
+    "url": "/google-places-test/static/js/main.875a541c.chunk.js"
+  },
+  {
+    "revision": "1aeed149f468fae392ce",
+    "url": "/google-places-test/static/js/runtime-main.a84ffd19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-places-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
